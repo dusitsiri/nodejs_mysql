a7945662f29df55ea89e229b1f1861058e3ce0da
dusitsiri <dusit.siri12@gmail.com>
2019-04-04 19:27:56 +0700

    modified: alter_table_db.js

--- DB/alter_table_db.js
+++ DB/alter_table_db.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
+  user: "root",
+  password: "",
   database: "mydb"
 });
 
@@ -11,7 +11,7 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
   var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY"; //แก้ไขข้อมูลใน table โดยใช้ alter
-  con.query(sql, function (err, result) {
+  con.query(sql, function (err, result) {   
     if (err) throw err;
     console.log("Table altered");
   });
