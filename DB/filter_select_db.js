0271e98b9b9e886082c5d6a17578b7e3f82fb570
dusitsiri <dusit.siri12@gmail.com>
2019-04-06 11:30:31 +0700

    edit file escape_query_db.js to orderby_db.js and edit filter_select_db.js

--- DB/filter_select_db.js
+++ DB/filter_select_db.js
@@ -27,13 +27,36 @@ var con = mysql.createConnection({
 //     });
 // });
 
-//escape query values, prevent sql injections
-con.connect(function(err){
-    if(err) throw err;
 
-    var addr = 'Mountain 21';
-    var sql = "select * from customers where address = " + mysql.escape(addr);
-    con.query(sql, function(err, result){
+//i mean like encapsulation
+//escape query values, prevent sql injections
+// con.connect(function(err){
+//     if(err) throw err;
+
+//     var addr = 'Mountain 21';
+//     var sql = "select * from customers where address = " + mysql.escape(addr);
+//     con.query(sql, function(err, result){
+//         if(err) throw err;
+//         console.log(result);
+//     });
+// });
+
+//use ? for the values you want to escape (placeholders)
+// con.connect(function(err){
+//     if (err) throw err;
+//     var addr = 'Mountain 21';
+//     var sql = 'select * from customers where address = ?';
+//     con.query(sql, [addr], function(err, result){
+//         if(err) throw err;
+//         console.log(result);
+//     });
+// });
+//multiple placeholders
+con.connect(function(err){
+    if (err) throw err;
+    var name = 'Amy';
+    var address = 'Mountain 21';
+    con.query("select * from customers where name = ? or address = ?", [name, address], function(err, result){
         if(err) throw err;
         console.log(result);
     });
