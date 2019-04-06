db87154a4d8675f4c412f9a0b8d8a96ac821d258
dusitsiri <dusit.siri12@gmail.com>
2019-04-06 11:17:20 +0700

    edit filter_select_db.js and create file escape_query_db.js

--- DB/filter_select_db.js
+++ DB/filter_select_db.js
@@ -7,10 +7,35 @@ var con = mysql.createConnection({
     database: "mydb",
 });
 
+//filter by where
+// con.connect(function(err){
+//     if (err) throw err;
+//     con.query("select * from customers where address = 'Park Lane 38'", function(err, result){
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// });
+
+//filter by like 
+// con.connect(function(err){
+//     if(err) throw err;
+    
+//     sql = "select * from customers where address like 'S%'";
+//     con.query(sql, function(err, result){
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// });
+
+//escape query values, prevent sql injections
 con.connect(function(err){
-    if (err) throw err;
-    con.query("select * from customers where address = 'Park Lane 38'", function(err, result){
-        if (err) throw err;
+    if(err) throw err;
+
+    var addr = 'Mountain 21';
+    var sql = "select * from customers where address = " + mysql.escape(addr);
+    con.query(sql, function(err, result){
+        if(err) throw err;
         console.log(result);
     });
 });
+
